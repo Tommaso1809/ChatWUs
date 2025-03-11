4ef93ca851e84bbe7abc45daceedb777c7bf2e8b
Messansio <a.messana2@studenti.unipi.it>
2025-03-11 17:09:36 +0100

    fixed modules

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,8 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 const serverPort  = 3000;
 
 app.listen(serverPort, () => {
-    console.log("Server started ---> ${"+serverPort+"}");
+    console.log(`Server started ---> ${serverPort}`);
 });
