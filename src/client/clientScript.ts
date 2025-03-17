4c79e286459b3c1d1ab5e0eb465ac60fb7bd1812
Tommaso1809 <tommasopolvere18@gmail.com>
2025-03-17 10:04:43 +0100

    Correct color

--- src/client/clientScript.ts
+++ src/client/clientScript.ts
@@ -3,7 +3,7 @@ const socket = io();
 
         function getUsernameColor(username) {
             if (!userColors[username]) {
-                const colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'];
+                const colors = ['#e6194b', '#3cb44b', '#ffe119', '#293d85', '#f58231', '#911eb4', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff', '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'];
                 userColors[username] = colors[Math.floor(Math.random() * colors.length)];
             }
             return userColors[username];
